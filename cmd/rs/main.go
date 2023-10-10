02c59e015b7352d21d001f31908485e58ecf78ad
Nestor <nestor@nstlopez.com>
2023-10-10 23:23:09 +0200

    simple version

---
+++ cmd/rs/main.go
@@ -0,0 +1,133 @@
+package main
+
+// An example Bubble Tea server. This will put an ssh session into alt screen
+// and continually print up to date terminal information.
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/log"
+	"github.com/charmbracelet/ssh"
+	"github.com/charmbracelet/wish"
+	bm "github.com/charmbracelet/wish/bubbletea"
+	lm "github.com/charmbracelet/wish/logging"
+)
+
+const (
+	host = "localhost"
+	port = 23234
+)
+
+func main() {
+	s, err := wish.NewServer(
+		wish.WithAddress(fmt.Sprintf("%s:%d", host, port)),
+		wish.WithHostKeyPath(".ssh/term_info_ed25519"),
+		wish.WithMiddleware(
+			bm.Middleware(teaHandler),
+			lm.Middleware(),
+		),
+	)
+	if err != nil {
+		log.Error("could not start server", "error", err)
+	}
+
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+	log.Info("Starting SSH server", "host", host, "port", port)
+	go func() {
+		if err = s.ListenAndServe(); err != nil && !errors.Is(err, ssh.ErrServerClosed) {
+			log.Error("could not start server", "error", err)
+			done <- nil
+		}
+	}()
+
+	<-done
+	log.Info("Stopping SSH server")
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer func() { cancel() }()
+	if err := s.Shutdown(ctx); err != nil && !errors.Is(err, ssh.ErrServerClosed) {
+		log.Error("could not stop server", "error", err)
+	}
+}
+
+// You can wire any Bubble Tea model up to the middleware with a function that
+// handles the incoming ssh.Session. Here we just grab the terminal info and
+// pass it to the new model. You can also return tea.ProgramOptions (such as
+// tea.WithAltScreen) on a session by session basis.
+func teaHandler(s ssh.Session) (tea.Model, []tea.ProgramOption) {
+	// Get the source IP address from the ssh.Session
+	remoteAddr := s.RemoteAddr().(*net.TCPAddr).IP
+
+	// Check if the source IP is within the allowed range
+	if !isAllowedIP(remoteAddr) {
+		wish.Fatalln(s, "Access denied from this IP address")
+		return nil, nil
+	}
+
+	// Get the user from the ssh.Session
+	user := s.User()
+
+	// Check if the user is 'rs' and deny access if it is
+	if user == "rs" {
+		wish.Fatalln(s, "Access denied for user rs")
+		return nil, nil
+	}
+
+	pty, _, active := s.Pty()
+	if !active {
+		wish.Fatalln(s, "no active terminal, skipping")
+		return nil, nil
+	}
+	m := model{
+		term:   pty.Term,
+		width:  pty.Window.Width,
+		height: pty.Window.Height,
+	}
+	return m, []tea.ProgramOption{tea.WithAltScreen()}
+}
+
+func isAllowedIP(ip net.IP) bool {
+	// Check if the IP is within the allowed range (10.42.0.0/16)
+	_, subnet, _ := net.ParseCIDR("10.42.0.0/16")
+	return subnet.Contains(ip)
+}
+
+// Just a generic tea.Model to demo terminal information of ssh.
+type model struct {
+	term   string
+	width  int
+	height int
+}
+
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.WindowSizeMsg:
+		m.height = msg.Height
+		m.width = msg.Width
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "q", "ctrl+c":
+			return m, tea.Quit
+		}
+	}
+	return m, nil
+}
+
+func (m model) View() string {
+	s := "Your term is %s\n"
+	s += "Your window size is x: %d y: %d\n\n"
+	s += "Press 'q' to quit\n"
+	return fmt.Sprintf(s, m.term, m.width, m.height)
+}
